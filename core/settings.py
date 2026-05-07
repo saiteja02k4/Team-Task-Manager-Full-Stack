0ccf116cf3873326a38d906481a85601974189f0
saiteja02k4 <pedaganisaiteja@gmail.com>
2026-05-07 15:48:52 +0530

    fix: update settings for render deployment

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", ".onrender.com").split(",")
+ALLOWED_HOSTS = [
+    "team-task-manager-full-stack-1-s3jc.onrender.com",
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
