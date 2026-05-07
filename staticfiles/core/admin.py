628e360b2e7aecec104f315e002c9368e9f23d04
saiteja02k4 <pedaganisaiteja@gmail.com>
2026-05-07 13:51:19 +0530

    fix duplicate django core structure

--- staticfiles/core/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-admin.site.site_header = "Team Task Manager Admin"
-admin.site.site_title = "Team Task Manager"
-admin.site.index_title = "Welcome to Team Task Manager Dashboard"
--- staticfiles/core/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Team Task Manager By Pedagani Sai Teja")
